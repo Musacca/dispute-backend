8216c4c86ce21373cfd0ea840d40ca41a72bdda3
MusabirMM <mmmusabir@gmail.com>
2024-08-15 15:55:54 +0400

    dsdsdsdsds

--- src/main/java/com/kapital/dispute/model/response/Dispute.java
+++ src/main/java/com/kapital/dispute/model/response/Dispute.java
@@ -2,4 +2,97 @@ package com.kapital.dispute.model.response;
 
 public record Dispute(Long id, String number, String title) {
 
+    //public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}public record Dispute(Long id, String number, String title) {
+//
+//}
 }
